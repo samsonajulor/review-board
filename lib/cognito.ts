de263c97eef55bbd495015913dafdf4cb06b7c6a
samsonajulor <samsonajulor@gmail.com>
2025-01-07 05:57:51 +0100

    included cognito construct

---
+++ lib/cognito.ts
@@ -0,0 +1,26 @@
+import * as cognito from 'aws-cdk-lib/aws-cognito';
+import { Construct } from 'constructs';
+
+export class CognitoConstruct extends Construct {
+  public readonly userPool: cognito.UserPool;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.userPool = new cognito.UserPool(this, 'UserPool', {
+      userPoolName: 'ReviewBoardUserPool',
+      selfSignUpEnabled: true,
+      signInAliases: { email: true },
+      standardAttributes: { email: { required: true, mutable: true } },
+    });
+
+    new cognito.CfnUserPoolGroup(this, 'AdminGroup', {
+      userPoolId: this.userPool.userPoolId,
+      groupName: 'Admins',
+    });
+
+    new cognito.UserPoolClient(this, 'UserPoolClient', {
+      userPool: this.userPool,
+    });
+  }
+}
