88d48190650794d255af43c6be06b8f84621e49c
samsonajulor <samsonajulor@gmail.com>
2025-01-07 05:57:21 +0100

    included dynamodb construct

---
+++ lib/dynamo.ts
@@ -0,0 +1,23 @@
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export class DynamoDBConstruct extends Construct {
+  public readonly table: dynamodb.Table;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    this.table = new dynamodb.Table(this, 'ReviewsTable', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'createdAt', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    this.table.addGlobalSecondaryIndex({
+      indexName: 'SentimentIndex',
+      partitionKey: { name: 'sentiment', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'createdAt', type: dynamodb.AttributeType.STRING },
+    });
+  }
+}
